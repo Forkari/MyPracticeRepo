ff5735f7c78507be1833ca3776b0b9f1f084c021
Forkari <22051771@kiit.ac.in>
2025-07-05 12:50:02 +0530

    Add files
    Week 3: Entitiy Framework 8.0
    Lab 1, 2, 3, 4, 5

---
+++ Week3_EntityFramework8.0/Program.cs
@@ -0,0 +1,20 @@
+﻿using RetailInventory.Data;
+using RetailInventory.Models;
+using Microsoft.EntityFrameworkCore;
+
+using var context = new AppDbContext();
+
+var products = await context.Products.ToListAsync();
+Console.WriteLine("All Products:");
+foreach (var p in products)
+{
+    Console.WriteLine($"{p.Name} - {p.Price}");
+}
+
+var product = await context.Products.FindAsync(1);
+Console.WriteLine($"\nProduct with ID 1: {product?.Name}");
+
+var expensiveProduct = await context.Products.FirstOrDefaultAsync(p => p.Price > 50000);
+Console.WriteLine($"\nFirst expensive product (> 50000): {expensiveProduct?.Name}");
+
+Console.ReadLine(); 
