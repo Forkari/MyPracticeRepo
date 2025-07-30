6a9c0df7460a220b8a6c04b04563982aac4ed902
Forkari <22051771@kiit.ac.in>
2025-07-30 19:07:29 +0530

    Week6_React
    Exercise 1,2,3,4,5

---
+++ Week6_React/blogapp/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Posts from './Posts';
+
+function App() {
+  return (
+    <h1>Test</h1>
+  );
+}
+
+export default App;
---
+++ Week6_React/blogapp/src/Posts.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class Posts extends React.Component{
+    constructor(props)
+    {
+        super(props);
+    }
+    loadPosts()
+    {
+        //code
+    }
+    componentDidMount()
+    {
+        //code
+    }
+    render()
+    {
+        return(
+            <div> Return something</div>
+        );
+    }
+    componentDidCatch(error, info)
+    {
+        //code
+    }
+}
+
+export default Posts;
